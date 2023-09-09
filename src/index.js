651683f93682e358c8698f9a87e0f0b5e32563c9
Victor C. Panus III <victor.panus@panusventures.com>
2023-09-09 16:47:58 -0500

    Configure client side dependencies, webpack and react hello world

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App tab="home" />);
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = {
+  // Entry point for your application
+  entry: './src/index.js',
+
+  // Output configuration
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'static', 'dist')
+  },
+
+  // Module configuration
+  module: {
+    rules: [
+      // Babel loader for React JSX and ES6+ syntax
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      // CSS loader for Bootstrap and your own CSS
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // File loader for FontAwesome
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+};
